7cf04cb1baa9ad29aecdab50bd01be2017dbdd61
fanchy <zxfown@gmail.com>
2019-06-12 20:59:35 +0800

    dir

--- web/website/siteConfig.js
+++ web/website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'h2cloud', // Title for your website.
   tagline: 'framework for game server',
   url: 'https://fanchy.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  baseUrl: '/web/website/build/h2cloud/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
